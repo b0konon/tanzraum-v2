7f624da9d7a355f496b97f147ed85c5a6053230b
Nicolaas Ludwig <nicolaas.ludwig@gmail.com>
2025-09-09 17:59:31 +0200

    updated server url vor strapi

--- tanzraum-strapi/config/server.ts
+++ tanzraum-strapi/config/server.ts
@@ -1,6 +1,9 @@
+import { url } from "inspector";
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('SERVER_URL'),
   app: {
     keys: env.array('APP_KEYS'),
   },
