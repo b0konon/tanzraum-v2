944697533875e5b0df482d45c3398b081d2fcb4b
Nicolaas Ludwig <nicolaas.ludwig@gmail.com>
2025-09-10 05:32:59 +0200

    even more cors

--- tanzraum-strapi/config/middlewares.ts
+++ tanzraum-strapi/config/middlewares.ts
@@ -6,22 +6,9 @@ export default [
     name: 'strapi::cors',
     config: {
       origin: [
-        'http://localhost:3000',
-        'http://localhost:1337',
-        'http://91.98.137.7:3000',
-        process.env.FRONTEND_URL,
-        process.env.SERVER_URL,
+        '*'
       ],
-      methods: (ctx) => {
-        const origin = ctx.request.header.origin;
-        if (
-          origin === 'http://localhost:1337' ||
-          origin === process.env.SERVER_URL
-        ) {
-          return ['GET', 'POST', 'PUT', 'DELETE'];
-        }
-        return ['GET'];
-      },
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
       headers: '*',
       credentials: true,
     },
